9c6bc48fabfcbd66c0d1629b16fb933ce0a3ffd3
Yauhen Kuzora <Yauhen_Kuzora@epam.com>
2017-07-28 18:05:18 +0300

    add useless simple test based on junit api (as added dependency to local repo)

---
+++ src/test/java/com/epam/server/JUnitSimpleTest.java
@@ -0,0 +1,24 @@
+package com.epam.server;
+
+import org.junit.Test;
+import org.junit.Ignore;
+
+import static org.junit.Assert.assertEquals;
+
+public class JUnitSimpleTest {
+    private String message = "Robert";
+
+    @Test
+    public void testPrintMessage() {
+        message = "Robert";
+        assertEquals(message, this.message);
+    }
+
+    @Ignore
+    @Test
+    public void testSalutationMessage() {
+        message = "Hi!" + "Robert";
+        assertEquals(message, this.message);
+    }
+
+}
